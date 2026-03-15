81751966298bfa2258c071f5c2e8fee85a4321be
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-03-16 00:57:13 +0200

    return function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,24 +14,24 @@ export class AppComponent implements OnInit{
 
 
   ngOnInit(): void {
-     //  this.playWelcomeMessage();
+     this.playWelcomeMessage();
   }
 
-//  playWelcomeMessage() {
-//     const message = new SpeechSynthesisUtterance(
-//       'Hello, I am Engineer Shimaa. I am a tester, a Power BI developer, and I work at ITI.  i,m Freelance & Corporate Data Trainer and Data Analysis Instructor | Turning Learners into Analysts and Freelance & Corporate Data Trainer And I work Pioneer Solution '
-//     );
-//     message.lang = 'en-US';
-//     message.rate = 1;
-//     message.pitch = 1;
-//     const voices = speechSynthesis.getVoices();
-//     const selectedVoice = voices.find(v => v.lang === 'en-US' && v.name.toLowerCase().includes('female'));
-//     if (selectedVoice) {
-//       message.voice = selectedVoice;
-//     }
+ playWelcomeMessage() {
+    const message = new SpeechSynthesisUtterance(
+      'Hello, I am Engineer Shimaa. I am a tester, a Power BI developer, and I work at ITI.  i,m Freelance & Corporate Data Trainer and Data Analysis Instructor | Turning Learners into Analysts and Freelance & Corporate Data Trainer And I work Pioneer Solution '
+    );
+    message.lang = 'en-US';
+    message.rate = 1;
+    message.pitch = 1;
+    const voices = speechSynthesis.getVoices();
+    const selectedVoice = voices.find(v => v.lang === 'en-US' && v.name.toLowerCase().includes('female'));
+    if (selectedVoice) {
+      message.voice = selectedVoice;
+    }
 
-//     speechSynthesis.speak(message);
-//   }
+    speechSynthesis.speak(message);
+  }
 
 
 
